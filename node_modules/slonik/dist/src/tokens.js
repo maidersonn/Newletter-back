9cbb2b420f31e823a2b35278d19f5ed064ab9b57
maidersonn <maidersonn@gmail.com>
2022-02-14 10:22:27 +0100

    feat: configure db

---
+++ node_modules/slonik/dist/src/tokens.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UnnestToken = exports.SqlToken = exports.ListToken = exports.JsonToken = exports.IdentifierToken = exports.ComparisonPredicateToken = exports.BinaryToken = exports.ArrayToken = void 0;
+exports.ArrayToken = 'SLONIK_TOKEN_ARRAY';
+exports.BinaryToken = 'SLONIK_TOKEN_BINARY';
+exports.ComparisonPredicateToken = 'SLONIK_TOKEN_COMPARISON_PREDICATE';
+exports.IdentifierToken = 'SLONIK_TOKEN_IDENTIFIER';
+exports.JsonToken = 'SLONIK_TOKEN_JSON';
+exports.ListToken = 'SLONIK_TOKEN_LIST';
+exports.SqlToken = 'SLONIK_TOKEN_SQL';
+exports.UnnestToken = 'SLONIK_TOKEN_UNNEST';
+//# sourceMappingURL=tokens.js.map
