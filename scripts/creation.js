28318e4a9fb479849eeaf3dac935ca0b56cea9aa
maidersonn <maidersonn@gmail.com>
2022-02-16 11:38:56 +0100

    feat: add script for db creation

---
+++ scripts/creation.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const db = require("../config/db");
+const { sql } = require("slonik");
+const create = async () => {
+  try {
+    await db.query(sql`
+    CREATE TABLE IF NOT EXISTS subscribers (
+      username TEXT,
+      email TEXT UNIQUE NOT NULL,
+      token TEXT,
+    );
+    `);
+    console.info("> creation done! 🚀");
+  } catch (error) {
+    console.info("> creation error! ❌");
+    console.info(">", error.message);
+  }
+};
+
+create();
